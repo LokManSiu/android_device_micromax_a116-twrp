03fcdfdf252433ee82eed199a18d7dd1bf616ddb
neomanu <neuvine@gmail.com>
2014-12-30 01:48:21 +0530

    Correct wrong ABI

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,7 +11,7 @@ TARGET_BOARD_PLATFORM := mt6589
 # Architecture
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_ABI := armeabi
+TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
 TARGET_CPU_VARIANT := cortex-a7
